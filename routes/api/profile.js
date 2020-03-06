093a09bba26b7b1c8f1017c57f498a2f6bd1dd65
Richard Scott Prins Jr <richardsprins@gmail.com>
2020-03-05 23:40:13 -0800

    added profile model and current user auth endpoint

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,28 @@
 const express = require('express')
+const auth = require('../../middleware/auth')
 const router = express.Router()
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 
-// @route    GET api/profile
-// @desc     Test route
-// @access   Public
+// @route    GET api/profile/me
+// @desc     Get Current User's Profile route
+// @access   Private
 
-router.get('/', (req, res) => res.send('Profile Route'))
+router.get('/me', auth, async (req, res) => {
+
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar'])
+
+    if(!profile){
+      return res.status(400).json({ message: "There is no profile for this user" })
+    }
+
+    res.json(profile)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500)
+    res.send('Server Error')
+  }
+})
 
 module.exports = router
