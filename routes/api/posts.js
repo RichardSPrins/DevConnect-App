d21b135568c1b3f0929ae9c35db41ec191c550e0
Richard Scott Prins Jr <richardsprins@gmail.com>
2020-03-16 07:56:54 -0700

    Completed: Adding DELETE route for posts. Next Task: Comments routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,145 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
+const auth = require('../../middleware/auth')
 
-// @route    GET api/posts
+const User = require('../../models/User')
+const Post = require('../../models/Post')
+const Profile = require('../../models/Profile')
+
+
+
+// @route    POST api/posts
+// @desc     Makes a new Post
+// @access   Private
+
+router.post('/', 
+  [
+    auth, 
+    [
+      check('text', 'Post body is required.').not().isEmpty()
+    ]
+  ] ,
+  async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+      return res.status(400).json({ errors: errors.array()})
+    }
+
+
+    try {
+      // get user by id
+      const user = await User.findById(req.user.id).select('-password')
+
+      //  Build new Post object
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      })
+
+      // save post to database 
+      const post = await newPost.save()
+      res.json(post)
+
+    } catch (error) {
+      console.error(error)
+      res.status(500).send('Server Error')
+    }
+  }
+)
+
+
+// @route    GET api/posts/
+// @desc     Get All Posts
+// @access   Private
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1})
+    res.json(posts)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Server Error')
+  }
+})
+
+
+// @route    GET api/posts/:post_id
+// @desc     Get post by id
+// @access   Private
+
+router.get('/:post_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id)
+
+    if(!post){
+      return res.status(404).json({ message: "Post not found."})
+    }
+
+    res.json(post)
+  } catch (error) {
+    console.error(error.message)
+    if(error.kind === 'ObjectId'){
+      return res.status(404).json({ message: "Post not found."})
+    }
+    res.status(500).send('Server Error')
+  }
+})
+
+
+// @route    PUT api/posts
 // @desc     Test route
 // @access   Public
 
-router.get('/', (req, res) => res.send('Posts Route'))
+router.put('/', (req, res) => res.send('Posts Route'))
+
+
+// @route    PUT api/posts/:post_id/comments/:comment_id
+// @desc     Test route
+// @access   Public
+
+router.put('/', (req, res) => res.send('Posts Route'))
+
+
+// @route    DELETE api/posts/:post_id
+// @desc     Delete a post by ID
+// @access   Public
+
+router.delete('/:post_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id)
+    
+    if(!post){
+      return res.status(404).json({ message: "Post not found."})
+    }
+
+    // Check and validate user
+    if(post.user.toString() !== (req.user.id)){
+      return res.status(401).json({ message: 'User not authorized'})
+    }
+
+    await post.remove()
+
+    res.json({ 
+      message: 'post removed',
+      removedPost: post
+    })
+  } catch (error) {
+    console.error(error)
+    if(error.kind === 'ObjectId'){
+      return res.status(404).json({ message: "Post not found."})
+    }
+    res.status(500).send('Server Error')
+  }
+})
+
+
+// @route    DELETE api/posts/:post_id/comments/:comment_id
+// @desc     Test route
+// @access   Public
+
+router.delete('/', (req, res) => res.send('Posts Route'))
 
 module.exports = router
