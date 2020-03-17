705c777df448b338cfbb465bfd87b863752a88e1
Richard Scott Prins Jr <richardsprins@gmail.com>
2020-03-17 11:55:15 -0700

    added comment and like routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -181,27 +181,32 @@ router.put('/unlike/:post_id',
     }
 })
 
-// @route    PUT api/posts/:post_id/comments
+// @route    PUT api/posts/comments/:post_id
 // @desc     Post new comment to a specific post
 // @access   Private
 
-router.post('/:post_id/comments',
+router.put('/comments/:post_id',
   auth, 
   [
     check('text', 'Comment text is required.').not().isEmpty()
   ],
   async (req, res) => {
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-      return res.status(400).json({ errors: errors.array()})
-    }
-
     try {
-
       const user = await User.findById(req.user.id).select('-password')
+      const post = await Post.findById(req.params.post_id)
       
 
-      // const newComment
+      const newComment = {
+        text: req.body.text,
+        user: req.user.id,
+        avatar: user.avatar,
+        name: user.name
+      }
+
+      post.comments.unshift(newComment)
+      await post.save()
+
+      res.json(post.comments)
     } catch (error) {
       console.error(error.message)
       res.status(500).send('Server Error.')
@@ -209,11 +214,40 @@ router.post('/:post_id/comments',
   })
 
 
-// @route    PUT api/posts/:post_id/comments/:comment_id
-// @desc     Test route
+// @route    PUT api/posts/comments/:post_id/:comment_id
+// @desc     Delete a comment by comment and post id
 // @access   Public
 
-router.put('/', (req, res) => res.send('Posts Route'))
+router.delete('/comments/:post_id/:comment_id',
+  auth,   
+  async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.post_id)
+      // grab comment from post
+      const comment = post.comments.find(comment => comment.id === req.params.comment_id)
+
+      if(!comment) {
+        return res.status(404).json({ message: 'Comment not found.' })
+      }
+
+      // check if user and comment user match
+      if(comment.user.toString() !== req.user.id){
+        return res.status(401).json({ message: 'User not authorized' })
+      }
+
+      const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id)
+      // save post to db with new like
+      post.comments.splice(removeIndex, 1)
+
+      await post.save()
+
+      res.json(post.comments)
+
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send('Server Error.')
+    }
+})
 
 
 
