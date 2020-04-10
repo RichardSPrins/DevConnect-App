e3071fd5618fc44b5adcf1d42024f0df5fc9758c
Richard Scott Prins Jr <richardsprins@gmail.com>
2020-04-10 10:54:43 -0700

    starting front end, cleaning up initial components. NExt: Add React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Navbar from './Components/Layout/Navbar.component'
+
+const App = () => {
   return (
-    <div className="App">
+    <>
       <h1>DevConnect</h1>
-    </div>
+    </>
   );
 }
 
---
+++ client/src/Components/Layout/Navbar.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Login</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
