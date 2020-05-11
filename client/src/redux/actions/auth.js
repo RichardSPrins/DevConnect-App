6be9970b1b0959800e5e62e89915c1ad3ecac1c6
Richard Scott Prins Jr <richardsprins@gmail.com>
2020-05-11 15:05:38 -0700

    added register action and reducer, updated register component by connecting props and dispatch to Redux

---
+++ client/src/redux/actions/auth.js
@@ -0,0 +1,37 @@
+import {
+  REGISTER_START,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL
+} from './types'
+import { setAlert } from './alert'
+
+import axios from 'axios'
+
+// REGISTER USER action
+
+export const register = ({ name, email, password }) => async dispatch => {
+  const config = {
+    headers : {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const body = JSON.stringify({ name, email, password })
+
+  try {
+    const res = await axios.post('/api/users', body, config)
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data
+    })
+  } catch (error) {
+    console.log(error.response)
+    const errors = error.response.data.errors
+    if(errors){
+      errors.forEach(err => dispatch(setAlert(err.msg, 'danger')))
+    }
+    dispatch({
+      type: REGISTER_FAIL
+    })
+  }
+}
