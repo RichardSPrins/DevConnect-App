757e0fe33afa09c1322ebed70836368eb5f1775b
Richard Scott Prins Jr <richardsprins@gmail.com>
2020-05-11 09:53:36 -0700

    catching up commits

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import './App.css';
+// Component Imports
 import Navbar from './Components/Layout/Navbar.component'
+import Alert from './Components/Layout/Alert.component'
 import Landing from './Components/Layout/Landing.component'
+import Login from './Components/Auth/Login.component'
+import Register from './Components/Auth/Register.component'
+import { Route, Switch } from 'react-router-dom'
+// Style imports
+import './App.css';
+
+
 
 
 
@@ -9,7 +17,14 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <Landing />
+      <Route exact path="/" component={Landing}/>
+      <section className="container">
+        <Alert />
+        <Switch>
+          <Route exact path="/register" component={Register}/>
+          <Route exact path="/login" component={Login}/>
+        </Switch>
+      </section>
     </>
   );
 }
---
+++ client/src/Components/Auth/Login.component.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react'
+import { Link, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { setAlert } from '../../redux/actions/alert'
+
+
+import axios from 'axios'
+
+const Login = (props) => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const  { email, password } = formData
+  const { history, setAlert } = props;
+  const token = localStorage.getItem('x-auth-token')
+
+  const inputHandler = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const submitHandler = async e => {
+    e.preventDefault()
+    if(!password){
+      setAlert('Please enter your password!', 'danger')
+    } else {
+      // console.log(formData)
+      // console.log('Success!')
+      const user = {
+        email,
+        password
+      }
+      
+      try {
+        const config = {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+
+        const body = JSON.stringify(user)
+        const res = await axios.post('/api/auth', body, config)
+        console.log(res.data)
+        localStorage.setItem('x-auth-token', res.data.token)
+        history.push('/')
+      } catch (error) {
+        setAlert(error.response.data.errors[0].msg, 'danger')
+      }
+    }
+  }
+
+  if(token){
+    return <Redirect to='/'/>
+  }
+
+  return (
+    <>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+      <form className="form" onSubmit={e => submitHandler(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => inputHandler(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={e => inputHandler(e)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </>
+  )
+}
+
+Login.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+}
+
+
+export default connect(null, { setAlert })(Login)
